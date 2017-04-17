a7661e5a35e81f5da607c18f8265b72741661327
aarchoun <aymanearchoun@gmail.com>
2017-04-17 13:07:54 +0200

    fvfsdffsdfssdsqsdf

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,6 +16,10 @@
 
 package org.springframework.samples.petclinic;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
