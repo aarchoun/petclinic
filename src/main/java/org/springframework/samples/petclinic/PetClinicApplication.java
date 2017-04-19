69a7b488251f14fdb11b1e3a77af54eb1834f7d2
aarchoun <aymanearchoun@gmail.com>
2017-04-19 15:25:52 +0200

    commit777771

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,6 +27,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * PetClinic Spring Boot Application.
  * 
